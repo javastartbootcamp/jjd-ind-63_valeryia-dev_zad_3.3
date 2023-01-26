ad6cf731c135a7acb63c143ba7be2b37e1edd394
Valeryia Paliashchuk <vallery7543@gmail.com>
2023-01-26 22:29:12 +0100

    Poprawka

--- src/main/java/pl/javastart/task/Operations.java
+++ src/main/java/pl/javastart/task/Operations.java
@@ -1,27 +1,20 @@
 package pl.javastart.task;
 
 public class Operations {
-    boolean isEven(int a){
-      return a%2 == 0;
+    boolean isEven(int a) {
+        return a % 2 == 0;
     }
-    boolean isOdd(int a){
-        return a%2 != 0;
+
+    boolean isOdd(int a) {
+        return a % 2 != 0;
     }
-    void printInfoBoolean(boolean a){
-        System.out.println(a);
-    }
-    double circleField(double r){
+
+    double circleField(double r) {
         double pi = 3.14;
-        double field = pi*r*r;
-        return field;
+        return pi * r * r;
     }
-    void printInfoDouble(double a) {
-        System.out.println(a);
-    }
-    int power(int a){
-        return (int) Math.pow(a,2); //dlaczego tutaj było przetwarzać w int?
-    }
-    void printInfoInt(int a) {
-        System.out.println(a);
+
+    int power(int a) {
+        return (int) Math.pow(a, 2);
     }
 }
--- src/main/java/pl/javastart/task/TestOperations.java
+++ src/main/java/pl/javastart/task/TestOperations.java
@@ -1,12 +1,11 @@
 package pl.javastart.task;
 
 public class TestOperations {
-
     public static void main(String[] args) {
-    Operations op1 = new Operations();
-       op1.printInfoBoolean (op1.isEven(4));
-       op1.printInfoBoolean(op1.isOdd(3));
-       op1.printInfoDouble(op1.circleField(8.0));
-       op1.printInfoInt(op1.power(5));
+        Operations op1 = new Operations();
+        System.out.println(op1.isEven(4));
+        System.out.println(op1.isOdd(3));
+        System.out.println(op1.circleField(6.5));
+        System.out.println(op1.power(5));
     }
 }
